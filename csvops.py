1235c0a0bf74f40b29cc5e1d5d4001605e7e249d
rumeysayilmaz <era.ylmaz@gmail.com>
2021-07-08 15:14:44 +0300

    [ref] refactored codebase [add] added csv file creation

--- csvops.py
+++ csvops.py
@@ -1,9 +1,19 @@
 import csv
 
+csv_filename = 'serverdetails.csv'
+fieldnames = ['username', 'servername', 'ip']
+
+
+def create_csv_file():
+    server_csv_file = open(csv_filename, 'w', newline='')
+    server_settings_file_writer = csv.DictWriter(server_csv_file, fieldnames)
+    server_settings_file_writer.writeheader()
+    server_csv_file.close()
+
 
 def read_server_settings():
-    server_settings_file = open('serverdetails.csv')
-    # server_settings_dic_reader = csv.DictReader(server_settings_file, ['username', 'servername', 'ip'])
+    server_settings_file = open(csv_filename)
+    # server_settings_dic_reader = csv.DictReader(server_settings_file, fieldnames)
     server_settings_dic_reader = csv.DictReader(server_settings_file)
     for row in server_settings_dic_reader:
         print(row['username'], row['servername'], row['ip'])
@@ -12,13 +22,14 @@ def read_server_settings():
 
 
 def write_server_settings():
-    server_settings_file = open('serverdetails.csv', 'w', newline='')
-    server_settings_file_writer = csv.DictWriter(server_settings_file, ['username', 'servername', 'ip'])
+    server_settings_file = open(csv_filename, 'w', newline='')
+    server_settings_file_writer = csv.DictWriter(server_settings_file, fieldnames)
     server_settings_file_writer.writeheader()
     server_settings_file_writer.writerow({'username': 'x', 'servername': 'y', 'ip': '125.34.9.0'})
     server_settings_file.close()
 
 
 if __name__ == '__main__':
+    create_csv_file()
     read_server_settings()
     # write_server_settings()
